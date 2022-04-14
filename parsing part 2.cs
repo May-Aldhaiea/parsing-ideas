657f1d2ef91a7ed8c276820b9df2145eb2b00506
May-Aldhaiea <99053710+May-Aldhaiea@users.noreply.github.com>
2022-04-13 21:45:21 -0700

    Add files via upload

---
+++ parsing part 2.cs
@@ -0,0 +1,306 @@
+
+bool B, BD, D, DO, DI, E, ED, I, II, IN, IO, O, OA, OC, OD, OE, OF, OI, ON, OO, OP, OS, R, RA, S, SA, T, TC, V, VC, VS, X, XD;
+int id1, VCoutput;
+string outputID, convert;
+char command_loop()
+{
+  char h;
+  buffer.TryDequeue(out h);
+  
+  if (buffer.IsEmpty)
+  {
+    B = BD = D = DO = DI = E = ED = I = II = IN = IO = O = OA = 
+      OC = OD = OE = OF = OI = ON = OO = OP = OS = R = RA = S = 
+      SA = T = TC = V = VC = VS = X = XD = false;
+  }
+  if (D == true)
+  {
+  		switch(h)
+      {
+        case "I":
+        	port.Write{"0\r\n"}
+        	D = false;
+          DI = true;
+      		break;
+        case "O":
+        	D = false;
+          DO = true;
+        	break;
+      }
+    return h;
+  }
+  if (E == true)
+  {
+    switch(h)
+    {
+      case "D":
+        E = false;
+        // cancel downloading mode
+        break;
+    }
+    return h;
+  }
+  if (I == true)
+  {
+    switch(h)
+    {
+      case "I":
+    		port.Write{"0 x0\r\n"};
+        I = false;
+        break;
+      case "N":
+        I = false;
+        // clear ACL error code?
+        // OS status bit cleared?
+        break;
+      case "O":
+        I = false;
+        IO = true;
+        break;
+    }
+  }
+  
+  // "O" command
+	if(O == true)
+  {
+    switch(h)
+    {
+      case "A":
+        port.Write{"0,0\r\n"};
+        O = false;
+        break;
+      case "C":
+        port.Write{"0,0\r\n"};
+        O = false;
+        break;
+      case "D":
+        port.Write{"0 x0\r\n"};
+        O = false;
+        break;
+      case "E":
+        // ACL error code
+        O = false;
+        break;
+      case "F":
+        port.Write{"0,0\r\n"};
+        O = false;
+        break;
+      case "I":
+        // Port handler outputing "CCX-BLD9999Teleporter"
+        port.Write{"CCX-BLD9999Teleporter\r\n"};
+        O = false;
+        break;
+      case "N":
+        port.Write{"0 x0\r\n"};
+        O = false;
+        break;
+      case "O": 
+        port.Write{"0,0\r\n"};
+        O = false;
+        break;
+      case "P":
+        // selector
+        port.Write{"0\r\n"};
+        O = false;
+        break;
+      case "S";
+        // decimal integer in the range 0 through 1023
+        int code;
+        if (code == 8)
+        O = false;
+        break;
+      // case "V";
+      //   // varid with a bit size of 31
+      //   break;
+        
+    }
+  }
+    
+  if (R == true)
+  {
+    if (h == "A")
+    {
+      RA = true;
+      R = false;
+    }
+    return h;
+  }
+  if (S == true)
+  {
+    if (h == "A")
+    {
+      S = false;
+      SA = true;
+    }
+    return h;
+  }
+  if (T == true)
+  {
+    if (h == "C")
+    {
+      TC = true;
+      T = false;
+    }
+    return h;
+  }
+  if (V == true)
+  {
+    if (h == "C")
+    {
+      VC = true;
+      V = false;
+    }else if (h == "S")
+    {
+      VS = true;
+      V = false;
+    }
+    return h;
+  }
+  if (X == true)
+  {
+    if (h == "D")
+    {
+      X = false;
+      XD = true;
+    }
+    return h;
+  }
+  if (XD == true)
+  {
+    XD = false;
+    switch(id)
+    {
+      case 0:
+        break;
+      case 1:
+        break;
+      case 2:
+        break;
+      case 3:
+        break;
+      case 4:
+        break;
+      case 0:
+        break;
+      case 5:
+        break;
+      case 6:
+        break;
+      case 7:
+        break;
+      case 8:
+        break;
+      case 9:
+        break;
+      case 10:
+        break;
+    }
+    return h;
+  }
+  if (TC == true)
+  {
+    if (opcode == 2)
+    {
+      port.Write{"0.0\r\n"};
+    }else if (opcode == 42)
+    {
+      port.Write{"LCO\r\n"};
+      port.Write{"HCO\r\n"};
+    }else if (opcode == 4 || opcode == 43)
+    {
+      port.Write{"0\r\n"};
+    }
+    TC = false;
+    return h;
+  }
+  if (VC == true)
+  {
+    // capture output identifcation
+    convert = Regex.Match(outputID, @"\d+").Value;
+    VCoutput = Int32.Parse(convert);
+    VC = false;
+    return h;
+  }
+  if (VS == true)
+  {
+    
+  }
+  if (SA == true)
+  {
+     if (/* operation is omitted */)
+     {
+       SA = false;
+       port.Write{"0 x0\r\n"};
+     }
+    return h;
+  }
+  if (RA == true)
+  {
+    // chan samples?
+    port.Write{"0\r\n"};
+    RA = false;
+    return h;
+  }
+  
+  if (IO == true)
+  {
+    if(h == "-1")
+    {
+      	port.Write{"0 x0\r\n"};
+    }
+    return h;
+  }
+
+  }
+  if (DI == true)
+  {
+    // This command reads the status of the digital bit number specified
+    return h;
+  }
+  if (DO == true)
+  {
+    if (h == "1")
+    {
+      // outputs shadow registers to a host computer in decimal
+      port.Write{"0,0\r\n"}
+      return h;
+    }
+    // outputs shadow registers to a host in hexadecimal 
+    port.Write{"0,0\r\n"}
+    return h;
+  }
+  switch(h)
+  {
+    case "B":
+      B = true;
+      break;
+    case "D":
+      D = true;
+      break;
+    case "E":
+      E = true;
+      break;
+    case "I":
+      I = true;
+      break;
+    case "O":
+      O = true;
+      break; 
+    case "R":
+      R = true;
+      break;
+    case "S":
+      S = true;
+      break;
+    case "T":
+      T = true;
+      break;
+    case "V":
+      V = true;
+      break;
+    case "X":
+      X = true;
+      break;
+  }
+  return h;
+}
